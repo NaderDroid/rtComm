5f7df7c1ae21132f38fd188a3926b54a4244a9e1
Nader <na511dr@hotmail.com>
2021-02-21 00:20:08 +0300

    added client side socket and upgrade server to support socketConn

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require("express")
+const express = require("express");
+const socket = require("socket.io");
 var app = express();
+let isConnected = false;
 
-app.listen(4000 , function (){
+var server = app.listen(4000 , function (){
     console.log("Your server is running on port 4000")
     }
 )
-app.use(express.static("public"))
+app.use(express.static("public"));
+var sockServer = socket(server);
+
+sockServer.on("connection" , () => {
+    isConnected = true;
+    console.log("Web socket is connected" , socket.id)
+})
